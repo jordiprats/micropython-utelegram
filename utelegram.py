e9cdfb9b8d1eef75aa9c938413ff86f4605d6b80
Jordi Prats <jordi.prats@gmail.com>
2020-09-08 12:40:48 +0200

    message offset

--- utelegram.py
+++ utelegram.py
@@ -13,19 +13,13 @@ class ubot:
 
         self.default_handler = None
 
-        self.upd = {
-            'offset': 0,
-            'limit': 1,
-            'timeout': 30,
-            'allowed_updates': ['message']}
-
-        self.last_read = 0
+        self.message_offset = 0
 
         messages = self.read_messages()
         if messages:
             for message in messages:
-                if message['update_id'] > self.last_read:
-                    self.last_read = message['update_id']
+                if message['update_id'] > self.message_offset:
+                    self.message_offset = message['update_id']
 
 
     def send(self, chat_id, text):
@@ -39,13 +33,18 @@ class ubot:
 
     def read_messages(self):
         result = []
+        self.query_updates = {
+            'offset': self.message_offset,
+            'limit': 1,
+            'timeout': 30,
+            'allowed_updates': ['message']}
+
         try:
-            update_messages = urequests.post(self.url + '/getUpdates', json=self.upd).json() 
+            update_messages = urequests.post(self.url + '/getUpdates', json=self.query_updates).json() 
             if 'result' in update_messages:
                 for item in update_messages['result']:
                     if 'text' in item['message']:
                         result.append(item)
-                
             return result
         except:
             return None
@@ -60,9 +59,9 @@ class ubot:
         messages = self.read_messages()
         if messages:
             for message in messages:
-                if message['update_id'] > self.last_read:
+                if message['update_id'] > self.message_offset:
                     self.message_handler(message)
-                    self.last_read = message['update_id']
+                    self.message_offset = message['update_id']
     
     def register(self, command, handler):
         self.commands[command] = handler
