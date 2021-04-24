b613b1550a5c98cd4c37ca535eabbf005c47eb2a
jordiprats <jordi.prats@gmail.com>
2021-04-24 09:59:29 +0200

    Added configurable message offset

--- utelegram.py
+++ utelegram.py
@@ -6,18 +6,22 @@ import urequests
 
 class ubot:
     
-    def __init__(self, token):
+    def __init__(self, token, offset=0):
         self.url = 'https://api.telegram.org/bot' + token
         self.commands = {}
         self.default_handler = None
-        self.message_offset = 0
+        self.message_offset = offset
         self.sleep_btw_updates = 3
 
         messages = self.read_messages()
         if messages:
-            for message in messages:
-                if message['update_id'] > self.message_offset:
-                    self.message_offset = message['update_id']
+            if self.message_offset==0:
+                self.message_offset = messages[-1]['update_id']
+            else:
+                for message in messages:
+                    if message['update_id'] >= self.message_offset:
+                        self.message_offset = message['update_id']
+                        break
 
 
     def send(self, chat_id, text):
